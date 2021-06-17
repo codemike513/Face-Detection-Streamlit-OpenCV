99a276e28147a3e52eb8f6cbab713adddef9c66a
Mihir Pesswani <pesswani.mihir@gmail.com>
2021-06-17 20:57:08 +0530

    Changes

--- Face_Detection.py
+++ Face_Detection.py
@@ -97,7 +97,7 @@ def main():
         if image_file is not None:
             our_image = Image.open(image_file)
             st.text("Original Image")
-            st.image(our_image)
+            st.image(our_image, width=500)
 
         enhance_type = st.sidebar.radio(
             "Enhance Type", ["Original", "Gray-Scale", "Contrast", "Brightness", "Blurring"])
@@ -105,23 +105,23 @@ def main():
             new_img = np.array(our_image.convert('RGB'))
             img = cv2.cvtColor(new_img, 1)
             gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-            st.image(gray)
+            st.image(gray, width=500)
         if enhance_type == "Contrast":
             c_rate = st.sidebar.slider("Contrast", 0.5, 3.5)
             enhancer = ImageEnhance.Contrast(our_image)
             img_output = enhancer.enhance(c_rate)
-            st.image(img_output)
+            st.image(img_output, width=500)
         if enhance_type == "Brightness":
             c_rate = st.sidebar.slider("Brightness", 0.5, 3.5)
             enhancer = ImageEnhance.Brightness(our_image)
             img_output = enhancer.enhance(c_rate)
-            st.image(img_output)
+            st.image(img_output, width=500)
         if enhance_type == "Blurring":
             new_img = np.array(our_image.convert('RGB'))
             blur_rate = st.sidebar.slider("Blur", 0.5, 3.5)
             img = cv2.cvtColor(new_img, 1)
             blur_img = cv2.GaussianBlur(img, (11, 11), blur_rate)
-            st.image(blur_img)
+            st.image(blur_img, width=500)
 
         # Face Detection
         task = ["Faces", "Smiles", "Eyes", "Cannize", "Cartonize"]
@@ -130,24 +130,24 @@ def main():
 
             if feature_choice == 'Faces':
                 result_img, result_faces = detect_faces(our_image)
-                st.image(result_img)
+                st.image(result_img, width=500)
                 st.success("Found {} faces".format(len(result_faces)))
 
             elif feature_choice == 'Eyes':
                 result_img = detect_eyes(our_image)
-                st.image(result_img)
+                st.image(result_img, width=500)
 
             elif feature_choice == 'Smiles':
                 result_img = detect_smiles(our_image)
-                st.image(result_img)
+                st.image(result_img, width=500)
 
             elif feature_choice == 'Cannize':
                 result_img = cannize_image(our_image)
-                st.image(result_img)
+                st.image(result_img, width=500)
 
             elif feature_choice == 'Cartonize':
                 result_img = cartonize_image(our_image)
-                st.image(result_img)
+                st.image(result_img, width=500)
 
     elif choice == 'About':
         st.subheader("About")
